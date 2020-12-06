32a442e2c555027dcd4bc58a67e17943996bf623
Andrey Postylyakov <andrey_postylyakov@epam.com>
2020-12-07 01:06:25 +0300

    task-08 added cart api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://ydrurck6qi.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://ryygla05ak.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ydrurck6qi.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'https://6hkajlj2zd.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
